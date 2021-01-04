dc660193063a9c561dd6e7548b504cadcd1113fd
金龙 陈 <chenjinlong123@live.com>
2021-01-04 11:13:00 +0800

    Completed Tridiagonal_Circle

--- Direct_solution_of_equations/main.cpp
+++ Direct_solution_of_equations/main.cpp
@@ -7,18 +7,22 @@ int main()
 	using namespace matrix;
 	using namespace lineqa;
 	using namespace std;
-	Mat A(1, 5), B(1, 5), C(1, 5),b(5,1), x(5, 1);
+	Mat A(1, 5), B(1, 5), C(1, 5),b(5,1), x(5, 1),a(5,5);
 	double numA[5] = { 1,2,-3,4,-5 };
 	double numB[5] = { 1,3,4,7,6 };
 	double numC[5] = { 1,2,1,2,1 };
 	double numb[5] = { 5,9,2,19,-4 };
+	double numa[25] = { 1,1,0,0,1,2,3,2,0,0,0,-3,4,1,0,0,0,4,7,2,1,0,0,-5,6 };
 	//double numx[3] = { 0, 0, 0 };
 	A = numA;
 	B = numB;
 	C = numC;
 	b = numb;
+	a = numa;
 	//x = numx;
 	x = Tridiagonal_Circle(A, B, C, b);
 	cout << x << endl;
+	cout << Doolittle(a) << endl;
+	cout << Doolittle_Solve(a,b) << endl;
 	return 0;
 }
